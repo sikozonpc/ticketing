85de93120d4653c9f8455883bc6618c3c26ab4c1
Tiago Taquelim <tiago.taquelim.ferreira@gmail.com>
2021-10-02 18:41:50 +0100

    feat: nats client in tickets

--- README.md
+++ README.md
@@ -4,4 +4,10 @@
 npm i
 skaffold dev
 ```
+### Secrets
+To setup the kubernetes secrets:
+```bash
+kubectl create secret generic jwt-secret --from-literal=JWT_KEY=132
+```
+
 open chrome and type "thisisunsafe"
--- services/tickets/src/index.ts
+++ services/tickets/src/index.ts
@@ -2,8 +2,10 @@ import { DatabaseConnectionError } from '@tfticketing/common';
 import 'express-async-errors';
 import mongo from './db/mongo';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const PORT = process.env.PORT ?? 3000;
+const NATS_SERVICE_URI = `http://nats-service:4222`;
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -13,6 +15,7 @@ const start = async () => {
     throw new Error('Env MONGO_URI not found!');
   }
   try {
+    await natsWrapper.connect('ticketing', 'TEMP-RANDOM-ID', NATS_SERVICE_URI);
     await mongo.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB!')
   } catch (error) {
