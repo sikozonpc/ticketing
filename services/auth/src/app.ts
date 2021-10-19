629273c1d7266016b49055735c8353d55abdae65
Tiago Taquelim <tiago.taquelim.ferreira@gmail.com>
2021-10-19 10:53:47 +0100

    fix signed cookie

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -13,9 +13,8 @@ app.set('trust proxy', true); // trust the Ingress Nginx load balancer
 
 app.use(cookieSession({
   // since JWTs are tempered protection we can opt-out of cookie encryption to allow other services to read the cookie
-  signed: process.env.NODE_ENV === 'production',
-  keys: [process.env.JWT!],
-  secure: !isTestEnv,
+  signed: false,
+  secure: false,
 }));
 routes(app);
 app.use(errorHandler);
